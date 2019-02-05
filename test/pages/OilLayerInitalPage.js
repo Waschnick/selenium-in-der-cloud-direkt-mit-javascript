b7e11c85caec258bac645f07e2b02e29012cb6d5
sebastian.waschnick <sebastian@waschnick.de>
2019-02-05 21:19:05 +0100

    WIP make more stylish #7

--- test/pages/OilLayerInitalPage.js
+++ test/pages/OilLayerInitalPage.js
@@ -8,27 +8,27 @@ OilLayerInitalPage.resetCookiesOnDemoPage = (browser) => {
   return browser
     .url(browser.globals.launch_url_host1 + 'demos/direct-integration.html')
     .deleteCookies();
-}
+};
 
 OilLayerInitalPage.openDemoPage = (browser) => {
   return browser
     .url(browser.globals.launch_url_host1 + 'demos/direct-integration.html')
     .useCss().waitForElementVisible(PAGE_BACKGROUND, 1000, false);
-}
+};
 
 OilLayerInitalPage.assertOilIsVisible = (browser) => {
   return browser
     .useXpath().waitForElementVisible(OIL_YES_BUTTON, 2000, false);
-}
+};
 
 OilLayerInitalPage.assertOilLayerNotPresent = (browser) => {
   return browser
     .useXpath().pause(500).waitForElementNotPresent(OIL_LAYER, 1000)
-}
+};
 
 OilLayerInitalPage.clickOnGiveConsent = (browser) => {
   return browser
     .useXpath().click(OIL_YES_BUTTON).pause(200)
-}
+};
 
 module.exports = OilLayerInitalPage;
